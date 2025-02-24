7d3cc140a98f40150f6da13b297de7feb4649482
Ronny Kristiansen <ronny.kristiansen@mattilsynet.no>
2025-02-24 12:43:52 +0100

    merge diverged code

--- lib-protobuf/build.rs
+++ lib-protobuf/build.rs
@@ -3,6 +3,9 @@ fn main() -> Result<()> {
     prost_build::compile_protos(
         &[
             "src/protos/v1/event.proto",
+            "src/protos/v1/oppfolging.proto",
+            "src/protos/v1/status.proto",
+            "src/protos/v1/identifisering.proto",
             "src/protos/v1/rodtkjott/rodtkjott.proto",
             "src/protos/v1/rodtkjott/observasjon.proto",
             "src/protos/v1/rodtkjott/funn.proto",
@@ -10,11 +13,8 @@ fn main() -> Result<()> {
             "src/protos/v1/virksomhet/tilsynsobjekt.proto",
             "src/protos/v1/virksomhet/slakteri.proto",
             "src/protos/v1/publikum/publikum.proto",
-            "src/protos/v1/identifisering.proto",
             "src/protos/v1/vurdering/maskinvurdering.proto",
             "src/protos/v1/vurdering/manuell_vurdering.proto",
-            "src/protos/v1/oppfolging.proto",
-            "src/protos/v1/status.proto",
             "src/protos/v2/virksomhet/tilsynsobjekt.proto",
             "src/protos/v2/rodtkjott/funn.proto",
             "src/protos/v2/rodtkjott/rodtkjott.proto",
--- lib-protobuf/src/serialization.rs
+++ lib-protobuf/src/serialization.rs
@@ -1,5 +1,6 @@
+use bytes::Bytes;
 use prost::Message;
-use std::io::Cursor;
+pub use prost::{DecodeError, EncodeError};
 
 pub fn serialize<T: Message>(message: T) -> Vec<u8> {
     let mut buf = Vec::with_capacity(message.encoded_len());
@@ -8,5 +9,6 @@ pub fn serialize<T: Message>(message: T) -> Vec<u8> {
 }
 
 pub fn deserialize<T: Message + Default>(buf: &[u8]) -> Result<T, prost::DecodeError> {
-    T::decode(&mut Cursor::new(buf))
+    let bytes = Bytes::copy_from_slice(buf);
+    T::decode(bytes)
 }
