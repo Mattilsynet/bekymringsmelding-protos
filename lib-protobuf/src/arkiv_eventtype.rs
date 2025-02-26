51f1aea6122165fb32e239ef231e68fc84612d1e
Ronny Kristiansen <ronny.kristiansen@mattilsynet.no>
2025-02-26 15:42:26 +0100

    fix: diverge bm-arkiv add ArkivJobbAvskriv

--- lib-protobuf/src/arkiv_eventtype.rs
+++ lib-protobuf/src/arkiv_eventtype.rs
@@ -12,6 +12,7 @@ pub enum ArkivEventType {
     ArkivJobbBilderJournalpost,
     ArkivJobbSettSaksansvarlig,
     Unsupported(String),
+    ArkivJobbAvskriv
 }
 
 const RODKJOTT_ANKOMMET: &str = "no.mattilsynet.lib-schemas.protos.Rodtkjott.Ankommet";
@@ -19,7 +20,8 @@ const RODKJOTT_AVSLUTTET: &str = "no.mattilsynet.lib-schemas.protos.Avsluttet";
 const ARKIVJOBB_AVSLUTTET_DOKUMENT: &str =
     "no.mattilsynet.lib-schemas.protos.ArkivJobbAvsluttetDokument";
 const ARKIVJOBB_AVSLUTT_SAK: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbAvsluttSak";
-const ARKIVJOBB_JOURNALPOST_FERDIG: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbJournalfoer";
+const ARKIVJOBB_JOURNALFOER: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbJournalfoer";
+const ARKIVJOBB_AVSKRIV: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbAvskriv";
 const ARKIVJOBB_DOKUMENT: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobDokument";
 const ARKIVJOBB_VEDLEGG: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbVedlegg";
 const ARKIVJOBB_BILDE_JOURNALPOST: &str =
@@ -36,7 +38,8 @@ impl EventTypeTrait for ArkivEventType {
             ArkivEventType::ArkivJobbDokument => ARKIVJOBB_DOKUMENT,
             ArkivEventType::ArkivJobbVedlegg => ARKIVJOBB_VEDLEGG,
             ArkivEventType::ArkivJobbAvsluttSak => ARKIVJOBB_AVSLUTT_SAK,
-            ArkivEventType::ArkivJobbJournalfoer => ARKIVJOBB_JOURNALPOST_FERDIG,
+            ArkivEventType::ArkivJobbJournalfoer => ARKIVJOBB_JOURNALFOER,
+            ArkivEventType::ArkivJobbAvskriv => ARKIVJOBB_AVSKRIV,
             ArkivEventType::ArkivJobbBilderJournalpost => ARKIVJOBB_BILDE_JOURNALPOST,
             ArkivEventType::ArkivJobbSettSaksansvarlig => ARKIVJOBB_SETT_SAKSANSVARLIG,
             ArkivEventType::Unsupported(_) => "Unsupported",
@@ -51,7 +54,8 @@ impl From<&str> for ArkivEventType {
             RODKJOTT_AVSLUTTET => ArkivEventType::RodtkjottAvsluttet,
             ARKIVJOBB_AVSLUTTET_DOKUMENT => ArkivEventType::ArkivJobbAvsluttetDokument,
             ARKIVJOBB_AVSLUTT_SAK => ArkivEventType::ArkivJobbAvsluttSak,
-            ARKIVJOBB_JOURNALPOST_FERDIG => ArkivEventType::ArkivJobbJournalfoer,
+            ARKIVJOBB_JOURNALFOER => ArkivEventType::ArkivJobbJournalfoer,
+            ARKIVJOBB_AVSKRIV => ArkivEventType::ArkivJobbAvskriv,
             ARKIVJOBB_DOKUMENT => ArkivEventType::ArkivJobbDokument,
             ARKIVJOBB_VEDLEGG => ArkivEventType::ArkivJobbVedlegg,
             ARKIVJOBB_BILDE_JOURNALPOST => ArkivEventType::ArkivJobbBilderJournalpost,
