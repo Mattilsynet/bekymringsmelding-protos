5cd4fa4d83674c3fb6786f37054f22e62fa53115
Ronny Kristiansen <ronny.kristiansen@mattilsynet.no>
2025-02-26 14:44:48 +0100

    fix: diverge bm-arkiv add ArkivJobbVedlegg

--- lib-protobuf/src/arkiv_eventtype.rs
+++ lib-protobuf/src/arkiv_eventtype.rs
@@ -8,7 +8,7 @@ pub enum ArkivEventType {
     ArkivJobbAvsluttSak,
     ArkivJobbJournalfoer,
     ArkivJobbDokument,
-    ArkivJobbBilder,
+    ArkivJobbVedlegg,
     ArkivJobbBilderJournalpost,
     ArkivJobbSettSaksansvarlig,
     Unsupported(String),
@@ -21,7 +21,7 @@ const ARKIVJOBB_AVSLUTTET_DOKUMENT: &str =
 const ARKIVJOBB_AVSLUTT_SAK: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbAvsluttSak";
 const ARKIVJOBB_JOURNALPOST_FERDIG: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbJournalfoer";
 const ARKIVJOBB_DOKUMENT: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobDokument";
-const ARKIVJOBB_BILDE: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbBilde";
+const ARKIVJOBB_VEDLEGG: &str = "no.mattilsynet.lib-schemas.protos.ArkivJobbVedlegg";
 const ARKIVJOBB_BILDE_JOURNALPOST: &str =
     "no.mattilsynet.lib-schemas.protos.ArkivJobbBildeJournalpost";
 const ARKIVJOBB_SETT_SAKSANSVARLIG: &str =
@@ -34,7 +34,7 @@ impl EventTypeTrait for ArkivEventType {
             ArkivEventType::RodtkjottAvsluttet => RODKJOTT_AVSLUTTET,
             ArkivEventType::ArkivJobbAvsluttetDokument => ARKIVJOBB_AVSLUTTET_DOKUMENT,
             ArkivEventType::ArkivJobbDokument => ARKIVJOBB_DOKUMENT,
-            ArkivEventType::ArkivJobbBilder => ARKIVJOBB_BILDE,
+            ArkivEventType::ArkivJobbVedlegg => ARKIVJOBB_VEDLEGG,
             ArkivEventType::ArkivJobbAvsluttSak => ARKIVJOBB_AVSLUTT_SAK,
             ArkivEventType::ArkivJobbJournalfoer => ARKIVJOBB_JOURNALPOST_FERDIG,
             ArkivEventType::ArkivJobbBilderJournalpost => ARKIVJOBB_BILDE_JOURNALPOST,
@@ -53,7 +53,7 @@ impl From<&str> for ArkivEventType {
             ARKIVJOBB_AVSLUTT_SAK => ArkivEventType::ArkivJobbAvsluttSak,
             ARKIVJOBB_JOURNALPOST_FERDIG => ArkivEventType::ArkivJobbJournalfoer,
             ARKIVJOBB_DOKUMENT => ArkivEventType::ArkivJobbDokument,
-            ARKIVJOBB_BILDE => ArkivEventType::ArkivJobbBilder,
+            ARKIVJOBB_VEDLEGG => ArkivEventType::ArkivJobbVedlegg,
             ARKIVJOBB_BILDE_JOURNALPOST => ArkivEventType::ArkivJobbBilderJournalpost,
             ARKIVJOBB_SETT_SAKSANSVARLIG => ArkivEventType::ArkivJobbSettSaksansvarlig,
             other_str => ArkivEventType::Unsupported(other_str.to_string()),
